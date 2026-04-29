f43f33b7977c9b6b903889bfb1d2fdb89e39a963
Vishwajeet <vishwajeetku987@gmail.com>
2026-04-30 01:58:24 +0530

    Adding input field in Auth modal

--- auth/src/Auth.jsx
+++ auth/src/Auth.jsx
@@ -5,7 +5,31 @@ function Auth() {
 
     return (
         <div className={styles.overlay}>
-            <div className={styles.modal}></div>
+            <div className={styles.modal}>
+                <div className={styles.header}>
+                    <h2 className={styles.title}>PhatSe ⚡️</h2>
+                    <p className={styles.subtitle}>Login / SignUp</p>
+                    <div style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        border: '1px solid gray',
+                        borderRadius: '6px',
+                        padding: '6px'
+                    }}>
+                        <span style={{ marginRight: '8px' }}>+91</span>
+
+                        <input
+                            type="tel"
+                            placeholder="Enter mobile number"
+                            style={{
+                                flex: 1,
+                                border: 'none',
+                                outline: 'none'
+                            }}
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
